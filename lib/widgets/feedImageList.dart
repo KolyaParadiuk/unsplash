66c18f2ac53ac03ff15b741f6df18a8ab263a906
Mykola Paradyuk <m.paradyuk@MacBook-Pro-Julia.local>
2021-04-24 23:34:00 +0300

    add flutter_staggered_grid_view

--- lib/widgets/feedImageList.dart
+++ lib/widgets/feedImageList.dart
@@ -4,6 +4,7 @@ import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:unsplash/hooks/theme.dart';
 import 'package:unsplash/models/photo.dart';
 import 'package:unsplash/widgets/feedImage.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
 class FeedImageList extends HookWidget {
   final List<Photo> images;
@@ -24,7 +25,6 @@ class FeedImageList extends HookWidget {
   @override
   Widget build(BuildContext context) {
     var scrollController = useState(ScrollController());
-    var theme = useTheme();
     void scrollListener() async {
       if (scrollController.value.position.extentAfter < 1000) {
         this.onEndReached();
@@ -44,46 +44,20 @@ class FeedImageList extends HookWidget {
     useEffect(() {
       scrollController.value.addListener(scrollListener);
     }, []);
-    List<List<Photo>> splitImagesToClumns() {
-      List<Photo> firstCol = [];
-      List<Photo> secondCol = [];
-      images.forEach((i) {
-        if (images.indexOf(i) % 2 == 1) {
-          firstCol.add(i);
-          return;
-        }
-        secondCol.add(i);
-      });
-      return [firstCol, secondCol];
-    }
 
-    imageColumn(List<Photo> images) {
-      return Column(
-        children: images.map((i) => FeedImage(image: i)).toList(),
-      );
-    }
+
+
 
     return RefreshIndicator(
           onRefresh: () async {
           return await this.onRefresh();
         },
-        child: SingleChildScrollView(
-        physics: AlwaysScrollableScrollPhysics(),
-        controller: scrollController.value,
-        child: Container(
-          color: theme.primaryColorLight,
-          padding: EdgeInsets.only(left: 5, right: 5,top:MediaQuery.of(context).padding.top),
-          child: Column(
-            children: [
-              (Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children:
-                    splitImagesToClumns().map((ic) => imageColumn(ic)).toList(),
-              )),
-            ],
-          ),
-        ),
-      ),
+        child: StaggeredGridView.count(
+          controller: scrollController.value,
+          crossAxisCount: 4,
+          children: images.map((i) => FeedImage(image: i)).toList(),
+          staggeredTiles: images.map((i) => StaggeredTile.fit(2)).toList(),
+          )
     );
   }
 }
